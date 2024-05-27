276d6ca6f6d5b1edd7b72653f2056699bf4613c2
Jakub Stolarczyk <jakub1.stolarczyk@gmail.com>
2024-05-27 18:18:08 +0200

    paddle object created without functionalities

--- main.py
+++ main.py
@@ -5,5 +5,24 @@ window = t.screen
 window.title("Pong")
 window.setup(width=1000, height=600)
 window.bgcolor("black")
-window.mainloop()
 
+t.color("white")
+t.shape("square")
+t.shapesize(7,1)
+print(t.pos())
+t.penup()
+
+def up():
+    t.fd(30)
+
+
+t.goto(400,0)
+
+
+
+
+def wyglad():
+    t.pensize(5)
+
+
+window.mainloop()

58db194e68a2cc5e3b57cb929f16fb9f3a59d0fa
Jakub Stolarczyk <jakub1.stolarczyk@gmail.com>
2024-06-03 17:41:18 +0200

    paddle object created with functionalities

--- main.py
+++ main.py
@@ -6,23 +6,39 @@ window.title("Pong")
 window.setup(width=1000, height=600)
 window.bgcolor("black")
 
+HIGH_BORDER = 200
+LOW_BORDER = -200
+PADDLE_MOVEMENT = 30
+
+t.lt(90)
 t.color("white")
 t.shape("square")
-t.shapesize(7,1)
+t.shapesize(1,7)
 print(t.pos())
 t.penup()
 
 def up():
-    t.fd(30)
+    if t.pos()[1] > HIGH_BORDER:
+        pass
+    else:
+        t.fd(PADDLE_MOVEMENT)
+
+def down():
+    if t.pos()[1] < LOW_BORDER:
+        pass
+    else:
+        t.bk(PADDLE_MOVEMENT)
 
 
 t.goto(400,0)
 
+window.onkeypress(up, 'w')
+window.onkeypress(down, 's')
 
 
 
 def wyglad():
     t.pensize(5)
 
-
+window.listen()
 window.mainloop()
